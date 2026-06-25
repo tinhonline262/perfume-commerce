04efdcce37816b313b0dd11ce8313643f2dfe7db
Tinh Huynh <tinhonline262@gmail.com>
2026-06-25 22:52:58 +0700

    fix

--- frontend/src/constants/urlConstants.ts
+++ frontend/src/constants/urlConstants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8080";
+export const BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 export const API_BASE_URL = BASE_URL + "/api/v1";
 export const WEBSOCKET_URL = BASE_URL + "/websocket";
 
